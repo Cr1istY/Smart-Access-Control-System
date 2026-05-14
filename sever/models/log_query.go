4f5f32b48d2c196b23cb24a859233dad44a46fc8
crist <cr1st4ever@outlook.com>
2026-05-14 22:42:37 +0800

    feat(access log): 查询进入日志

--- sever/models/log_query.go
+++ sever/models/log_query.go
@@ -11,3 +11,14 @@ type LogQuery struct {
 	StartTime  time.Time // 开始时间（零值表示不限制）
 	EndTime    time.Time // 结束时间（零值表示不限制）
 }
+
+type LogQueryRequest struct {
+	UserID     string `form:"user_id"`
+	DeviceID   string `form:"device_id"`
+	Result     string `form:"result"`
+	AuthMethod string `form:"auth_method"`
+	StartTime  string `form:"start_time"` // 前端通常传字符串格式的时间，如 "2023-10-01 12:00:00"
+	EndTime    string `form:"end_time"`
+	Page       int    `form:"page" binding:"required,min=1"`      // 必填，且最小为1
+	PageSize   int    `form:"page_size" binding:"required,min=1"` // 必填，且最小为1
+}
